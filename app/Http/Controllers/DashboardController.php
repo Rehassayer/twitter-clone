a0ad85e0a73552177aee0a695fa65cf62e8636d2
Rehassayer <rakeshayer0@gmail.com>
2026-03-26 12:48:51 +0545

    twitter clone project begine

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Idea;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function index(){
+
+   $idea = new Idea([
+    'content' => 'Hello',
+    'likes' => 10
+   ]);
+   $idea->save();
+   return view('dashboard',[
+    'ideas' => idea::orderBy('created_at','DESC')->get()
+   ]);
+    }
+    
+}
